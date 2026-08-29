7ce672c6670c9c3147894b477dd71a972f0af25e
A1-X1 <daa237@outlook.com>
2026-08-30 00:59:24 +0100

    - Updating inditial code: - creates a socket - binds to free port - sets up receiving timeout

--- temp/probe.cpp
+++ temp/probe.cpp
@@ -1,3 +1,66 @@
 //
 // Created by Daanyar on 27/08/2026.
 //
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    // create the UDP socket
+    // first param is for ipv4 since WS discovery is there
+    // second param is the type, either UDP or TCP, in our case for the probe it is UDP
+    // third param is for the protocol
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // error handling
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // bind to any port locally for replies
+    // selects correct internet family
+    // OS selects any free port since set to 0
+    sockaddr_in local_addr{};
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    local_addr.sin_port = 0;
+
+    // check if can bind successfully and cast to sockaddr type
+    if (bind(sock, (sockaddr*)&local_addr, sizeof(local_addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    // set a receiving timeout
+    timeval tv{};
+    tv.tv_sec = 3;
+    tv.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+    std::string probe =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        "<e:Envelope xmlns:e=\"http://www.w3.org/2003/05/soap-envelope\" "
+        "xmlns:w=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" "
+        "xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" "
+        "xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\">"
+        "<e:Header>"
+        "<w:MessageID>uuid:12345678-1234-1234-1234-123456789abc</w:MessageID>"
+        "<w:To e:mustUnderstand=\"1\">urn:schemas-xmlsoap-org:ws:2005:04:discovery</w:To>"
+        "<w:Action e:mustUnderstand=\"1\">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</w:Action>"
+        "</e:Header>"
+        "<e:Body>"
+        "<d:Probe><d:Types>dn:NetworkVideoTransmitter</d:Types></d:Probe>"
+        "</e:Body>"
+        "</e:Envelope>";
+
+
+
+
+    return 0;
+
+}
